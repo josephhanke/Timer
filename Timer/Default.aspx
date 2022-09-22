2b0bef354e0ae99099b5486eb135587c355185d9
josephhanke <jhanke@nescot.ac.uk>
2022-09-22 14:32:31 +0100

    Add project files.

---
+++ Timer/Default.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Timer.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <asp:Timer ID="tmr1" runat="server" Enabled="False" Interval="1000" OnTick="tmr1_Tick">
+        </asp:Timer>
+        <br />
+        Select your birthday:<br />
+        <br />
+        <asp:Calendar ID="bdayCalendar" runat="server" VisibleDate="2002-01-31"></asp:Calendar>
+        <asp:Button ID="btn1" runat="server" OnClick="btn1_Click" Text="Button" />
+        <br />
+        <asp:Label ID="lblResult" runat="server" Text="100"></asp:Label>
+        <br />
+        <asp:ScriptManager ID="ScriptManager1" runat="server">
+        </asp:ScriptManager>
+    </form>
+</body>
+</html>
---
+++ Timer/Default.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Timer
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void tmr1_Tick(object sender, EventArgs e)
+        {
+            DateTime myBirthday = bdayCalendar.SelectedDate;
+            TimeSpan myAge = DateTime.Now.Subtract(myBirthday);
+                       
+            lblResult.Text = " you have been alive for " +myAge.TotalSeconds + " Seconds"; 
+          
+        }
+
+        protected void btn1_Click(object sender, EventArgs e)
+        {
+            if (tmr1.Enabled == true)
+            {
+                tmr1.Enabled = false;
+            }
+            if (tmr1.Enabled == false)
+            {
+                tmr1.Enabled = true;
+            }
+         
+        }
+    }
+}
